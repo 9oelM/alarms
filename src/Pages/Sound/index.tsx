5846b0a89e5ee689ebb38a8e3925ff42e1933490
Joel M <hj923@hotmail.com>
2020-05-09 18:40:58 +0900

    feat: allow previewing sound on selection (#9)

--- src/Pages/Sound/index.tsx
+++ src/Pages/Sound/index.tsx
@@ -10,8 +10,9 @@ import { ButtonWithTitle } from "@/Components/Combi/ButtonWithTitle";
 import { padding25px } from "@/constants/padding";
 import { SelectAlarmSound, ToggleAlarmVibration } from "@/redux/actions/ALARM";
 import { RootState } from "@/redux/reducers";
-// @ts-ignore
-// import testSound from "@/assets/sounds/test.mp3";
+import { playAlarm } from '@/util/playAlarm';
+import { Player } from '@react-native-community/audio-toolkit';
+import { usePrevious } from '@/hooks/usePrevious';
 
 export const Sound: FC = () => {
   const { selectedAlarmSound, isAlarmVibrationEnabled } = useSelector(
@@ -20,7 +21,11 @@ export const Sound: FC = () => {
       isAlarmVibrationEnabled: s.Alarm.isAlarmVibrationEnabled,
     }),
   );
+
+  const prevSelectedAlarmSound = usePrevious(selectedAlarmSound);
+
   const dispatch = useDispatch();
+
   const alarmSoundsList = ALARM_SOUNDS.map(
     ({ displayedName, fileName }, idx, arr) => {
       const isActive =
@@ -48,6 +53,14 @@ export const Sound: FC = () => {
       Vibration.vibrate(1);
     }
   }, [isAlarmVibrationEnabled]);
+
+  useEffect(() => {
+    if (selectedAlarmSound.fileName === 'None' || prevSelectedAlarmSound === null) return;
+
+    const player: Player | null = playAlarm(selectedAlarmSound.fileName);
+
+    return () => player?.destroy();
+  }, [selectedAlarmSound]);
 
   return (
     <GeneralContainer title={SOUND}>
